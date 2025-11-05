0f9a9bc717043456570c5ca8f9a5e3d5f1a6a406
Petr Pivoňka <petrpiv1@gmail.com>
2025-11-05 17:28:17 +0100

    Check for existing image

--- BlenderPlugin/texture.py
+++ BlenderPlugin/texture.py
@@ -1,10 +1,15 @@
+import os
 import bpy
 
 def create(texture_dict, mat):
     image_path = texture_dict["ImagePath"]
 
     texture_node = mat.node_tree.nodes.new("ShaderNodeTexImage")
-    texture_node.image = bpy.data.images.load(image_path)
+
+    if os.path.exists(image_path):
+        texture_node.image = bpy.data.images.load(image_path, check_existing=True)
+    # else report
+
     texture_node.hide = True
 
     return texture_node
