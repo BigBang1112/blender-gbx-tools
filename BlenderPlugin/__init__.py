f3db2094836114a5452bafa588e4c9295e1f2317
Petr Pivoňka <petrpiv1@gmail.com>
2026-01-04 04:29:11 +0100

    Fix dot

--- BlenderPlugin/__init__.py
+++ BlenderPlugin/__init__.py
@@ -14,7 +14,7 @@
 bl_info = {
     "name": "Blender Gbx Tools",
     "author": "BigBang1112",
-    "description": "Working with Gbx files inside of Blender.",
+    "description": "Working with Gbx files inside of Blender",
     "blender": (4, 1, 0),
     "version": (0, 1, 0),
     "location": "File > Import",
